32e9576713419be9b2407835e5fbd0ef91e2162f
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-19 03:09:05 +0900

    updated token with chainlink

---
+++ contracts/TokenFactoryWithData.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./TokenWithData.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract TokenFactory {
+    address immutable tokenImplementation;
+    address[] public tokens;
+    constructor() {
+        tokenImplementation = address(new HorseToken( address(bytes20(bytes("0xb83E47C2bC239B3bf370bc41e1459A34b41238D0"))), bytes32(bytes("0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000"))));
+    }
+    
+    /**
+     * 
+     * @param name name of the horse 
+     * @param symbol first 3 letters of name 
+     * @param _price horse price in eth
+     */
+    function createToken(string calldata name, string calldata symbol, uint256 _price) external returns (address) {
+        address clone = Clones.clone(tokenImplementation);
+        HorseToken(clone).initialize(name, symbol, msg.sender, _price);
+        tokens.push(clone);
+        return clone;
+    }
+}
---
+++ contracts/TokenWithData.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+
+contract HorseToken is ERC20Upgradeable, FunctionsClient, ConfirmedOwner{
+    address horseOwner;
+    uint256 maxSupply;
+    uint256 price;
+
+    bytes32 public donId;
+
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+
+    event ETHWithdrawn(uint256 amount);
+    event TokenPurchase(uint256 amount, address buyer);
+
+    constructor(address router, bytes32 _donId) FunctionsClient(router) ConfirmedOwner(msg.sender) {
+        donId = _donId;
+    }
+
+    function initialize(string calldata name, string calldata symbol, address _owner, uint256 _price) initializer public {
+        __ERC20_init(name, symbol);
+        horseOwner = _owner;
+        maxSupply = 100e18;
+        price = _price; // price in eth for 1 token (18 decimals)
+    }
+
+    function mintTokens() payable external{
+        uint256 tokenAmount = (msg.value / price); // ~ 1e18 / ~ 1 = 1e18
+        require(
+            tokenAmount + totalSupply() <= maxSupply,
+            "Reached the current supply limit, check the remaining amount!" 
+        );
+        emit TokenPurchase(tokenAmount, _msgSender());
+    }
+
+    /**
+     * @param amount in wei
+     */
+    function withdrawEth(uint256 amount) external {
+        require(msg.sender == horseOwner, "Not owner");
+        (bool sent, bytes memory data) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+        emit ETHWithdrawn(amount);
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+        s_lastResponse = response;
+        s_lastError = err;
+    }
+}
