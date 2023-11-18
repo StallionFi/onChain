b0d43ff3d0d6f3f01fab963b48660c8edfb3cbb5
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-18 20:03:26 +0900

    added events, cleaned up

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,14 +3,13 @@
 pragma solidity ^0.8.20;
 
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HorseToken is ERC20Upgradeable {
     address owner;
     uint256 maxSupply;
     uint256 price;
 
-    event ETHWithdrawn(bool success, uint256 amount);
+    event ETHWithdrawn(uint256 amount);
     event TokenPurchase(uint256 amount, address buyer);
 
     function initialize(string calldata name, string calldata symbol, address _owner, uint256 _price) initializer public {
@@ -26,7 +25,16 @@ contract HorseToken is ERC20Upgradeable {
             tokenAmount + totalSupply() <= maxSupply,
             "Reached the current supply limit, check the remaining amount!" 
         );
-        
         emit TokenPurchase(tokenAmount, _msgSender());
+    }
+
+    /**
+     * @param amount in wei
+     */
+    function withdrawEth(uint256 amount) external {
+        require(msg.sender == owner, "Not owner");
+        (bool sent, bytes memory data) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+        emit ETHWithdrawn(amount);
     }
 }
